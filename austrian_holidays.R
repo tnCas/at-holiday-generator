6a92e686a62dcae4edbca1a7760e2593fd52656d
Theo Okafor <tn.okafor@gmail.com>
2026-05-04 14:22:30 +0200

    Update R script with robust date handling

--- austrian_holidays.R
+++ austrian_holidays.R
@@ -1,45 +1,50 @@
-#' @title Austrian Holiday Generator
-#' @description Generates a vector of Austrian holidays and initializes a bizdays calendar.
+# at-holiday-generator
+# Logic for calculating movable Austrian feasts and business calendars
 
-# Load required libraries
+# Required Libraries
 if (!require("timeDate")) install.packages("timeDate")
 if (!require("bizdays")) install.packages("bizdays")
 
 library(timeDate)
 library(bizdays)
 
-# Calculation Functions
-get_easter_monday  <- function(y) as.Date(Easter(y)) + 1
-get_ascension      <- function(y) as.Date(Easter(y)) + 39
-get_whit_monday    <- function(y) as.Date(Easter(y)) + 50
-get_corpus_christi <- function(y) as.Date(Easter(y)) + 60
+# 1. Calculation Functions for Movable Feasts
+get_easter_date    <- function(year) as.Date(Easter(year))
+get_easter_monday  <- function(year) get_easter_date(year) + 1
+get_ascension      <- function(year) get_easter_date(year) + 39
+get_whit_monday    <- function(year) get_easter_date(year) + 50
+get_corpus_christi <- function(year) get_easter_date(year) + 60
 
-# Define Year Range
-start_year <- 2020
-end_year   <- 2030
+# 2. Configuration
+# Sets the range for holiday generation
+start_year <- as.numeric(format(Sys.Date(), "%Y"))
+end_year   <- start_year + 10
 
-# Generate Holiday Vector
-AT_HOLIDAYS <- as.Date(unlist(lapply(start_year:end_year, function(yr) {
+# 3. Generate Holiday List
+holiday_list <- lapply(start_year:end_year, function(yr) {
   c(
-    as.Date(paste0(yr, "-01-01")), # New Year
+    as.Date(paste0(yr, "-01-01")), # New Year's Day
     as.Date(paste0(yr, "-01-06")), # Epiphany
-    get_easter_monday(yr),
+    get_easter_monday(yr),         # Easter Monday
     as.Date(paste0(yr, "-05-01")), # Labour Day
-    get_ascension(yr),
-    get_whit_monday(yr),
-    get_corpus_christi(yr),
-    as.Date(paste0(yr, "-08-15")), # Assumption
-    as.Date(paste0(yr, "-10-26")), # National Holiday
-    as.Date(paste0(yr, "-11-01")), # All Saints
+    get_ascension(yr),             # Ascension Day
+    get_whit_monday(yr),           # Whit Monday
+    get_corpus_christi(yr),        # Corpus Christi
+    as.Date(paste0(yr, "-08-15")), # Assumption of the Virgin Mary
+    as.Date(paste0(yr, "-10-26")), # Austrian National Holiday
+    as.Date(paste0(yr, "-11-01")), # All Saints' Day
     as.Date(paste0(yr, "-12-08")), # Immaculate Conception
     as.Date(paste0(yr, "-12-24")), # Christmas Eve
     as.Date(paste0(yr, "-12-25")), # Christmas Day
-    as.Date(paste0(yr, "-12-26")), # St. Stephen's
+    as.Date(paste0(yr, "-12-26")), # St. Stephen's Day
     as.Date(paste0(yr, "-12-31"))  # New Year's Eve
   )
-})), origin = "1970-01-01")
+})
 
-# Create Calendar Object
+# Flatten and restore Date class
+AT_HOLIDAYS <- as.Date(unlist(holiday_list), origin = "1970-01-01")
+
+# 4. Initialize Custom Calendar
 create.calendar(
   name = "Austria", 
   holidays = AT_HOLIDAYS, 
